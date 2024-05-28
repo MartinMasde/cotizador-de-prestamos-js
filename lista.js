04471e1051909dd93cc04d3f7cd85718642afc1a
Martin Mas de Ayala <132627081+MartinMasde@users.noreply.github.com>
2024-05-28 16:14:47 -0300

    arreglos varios

--- lista.js
+++ lista.js
@@ -1,30 +1,34 @@
 // Cargar la lista de solicitantes al cargar la página
 document.addEventListener("DOMContentLoaded", () => {
     const listaSolicitantes = JSON.parse(localStorage.getItem("solicitantes")) || [];
+    // Verificar si hay solicitantes guardados
     const tbody = document.querySelector("#tablaSolicitantes tbody");
+        // Limpiar el contenido de la tabla
+        tbody.innerHTML = "";
+        // Iterar sobre la lista de solicitantes y agregar una fila por cada solicitante a la tabla
+        listaSolicitantes.forEach(solicitante => {
+            const tr = document.createElement("tr");
 
-    listaSolicitantes.forEach(solicitante => {
-        const tr = document.createElement("tr");
+            const tdNombre = document.createElement("td");
+            tdNombre.textContent = solicitante.nombre;
+            tr.appendChild(tdNombre);
 
-        const tdNombre = document.createElement("td");
-        tdNombre.textContent = solicitante.nombre;
-        tr.appendChild(tdNombre);
+            const tdApellido = document.createElement("td");
+            tdApellido.textContent = solicitante.apellido;
+            tr.appendChild(tdApellido);
 
-        const tdApellido = document.createElement("td");
-        tdApellido.textContent = solicitante.apellido;
-        tr.appendChild(tdApellido);
+            const tdDocumento = document.createElement("td");
+            tdDocumento.textContent = solicitante.documento;
+            tr.appendChild(tdDocumento);
 
-        const tdDocumento = document.createElement("td");
-        tdDocumento.textContent = solicitante.documento;
-        tr.appendChild(tdDocumento);
+            const tdEdad = document.createElement("td");
+            tdEdad.textContent = solicitante.edad;
+            tr.appendChild(tdEdad);
+            // Agregar la fila a la tabla
+            tbody.appendChild(tr);
+        } );
 
-        const tdEdad = document.createElement("td");
-        tdEdad.textContent = solicitante.edad;
-        tr.appendChild(tdEdad);
-
-        tbody.appendChild(tr);
-    });
-
+    // Event Listener para el boton de Volver
     document.getElementById("volver").addEventListener("click", () => {
         window.location.href = "index.html";
     });
--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const calcularPrestamo = () => {
   // Verificacion de que los valores ingresados sean válidos
   if (isNaN(monto) || monto <= 0) {
     // Mostrar aviso de error usando SweetAlert2
-    resultadoContainer.textContent = Swal.fire({
+    Swal.fire({
       title: "Debe ingresar un monto válido",
       icon: "error"
     });
@@ -16,7 +16,7 @@ const calcularPrestamo = () => {
   // Verificacion de que los valores ingresados sean válidos
   if (isNaN(cuotas) || cuotas <= 0) {
     // Mostrar aviso de error usando SweetAlert2
-    resultadoContainer.textContent = Swal.fire({
+    Swal.fire({
       title: "Debe ingresar una cantidad de cuotas válida",
       icon: "error"
     });
@@ -37,7 +37,7 @@ const calcularPrestamo = () => {
       break;
     default:
       // Mostrar aviso de error usando SweetAlert2
-      resultadoContainer.textContent = Swal.fire({
+      Swal.fire({
         title: "No se permiten más de 36 cuotas, ingrese nuevamente",
         icon: "error"
       });
@@ -46,7 +46,7 @@ const calcularPrestamo = () => {
  // Calculo del valor de cada cuota
   const valorCuota = (monto * (1 + recargo)) / cuotas;
   // Mostrar el valor de cada cuota en el HTML usando SweetAlert2
-  resultadoContainer.textContent = Swal.fire({
+  Swal.fire({
     title: "El valor de cada cuota es:",
     text: `$ ${valorCuota.toFixed(2)} `,
     icon: "success"
@@ -73,8 +73,8 @@ let listaSolicitantes = JSON.parse(localStorage.getItem("solicitantes")) || [];
 // Funcion para registrar solicitantes de prestamos
 const registrarSolicitante = (e) => {
   e.preventDefault();
+  // Obtener el valor de la verificacion, si o no
   let verificacion = document.querySelector(".registro").value;
-  const resultadoContainer = document.getElementById("resultado2");
 
   // Verificacion de que los valores ingresados sean válidos  
   if (verificacion === "si" 
@@ -86,6 +86,7 @@ const registrarSolicitante = (e) => {
     let apellido = document.getElementById("apellido").value;
     let documento = document.getElementById("documento").value;
     let edad = document.getElementById("edad").value;
+
     // Agregar un nuevo solicitante a la lista  
     listaSolicitantes.push(new Solicitante(nombre, apellido, documento, edad)); 
 
@@ -93,24 +94,24 @@ const registrarSolicitante = (e) => {
     localStorage.setItem("solicitantes", JSON.stringify(listaSolicitantes));
 
     // Mensaje de confirmación del registro de un solicitante usando SweetAlert2
-    resultadoContainer.textContent = Swal.fire({
-                                            position: "center",
-                                            icon: "success",
-                                            title: "Usuario registrado correctamente",
-                                            showConfirmButton: false,
-                                            timer: 2500
-                                          }).then(() => {
-                                            // Limpiar los inputs después de mostrar el mensaje de éxito
-                                            document.getElementById("nombre").value = "";
-                                            document.getElementById("apellido").value = "";
-                                            document.getElementById("documento").value = "";
-                                            document.getElementById("edad").value = "";
-                                          });
+    Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Usuario registrado correctamente",
+          showConfirmButton: false,
+          timer: 2500
+        }).then(() => {
+          // Limpiar los inputs después de mostrar el mensaje de éxito
+          document.getElementById("nombre").value = "";
+          document.getElementById("apellido").value = "";
+          document.getElementById("documento").value = "";
+          document.getElementById("edad").value = "";
+        });
 
   } else if (verificacion === "no") {
     return;
   } else {
-    // alert("Ingrese un valor válido");
+    // Mostrar aviso de error usando SweetAlert2 si 
     Swal.fire({
       title: "Registro de Solicitante",
       text: "Debe ingresar un valor válido",
@@ -121,58 +122,30 @@ const registrarSolicitante = (e) => {
 // Event Listener para el botón de registrar solicitante
 document.getElementById("registrar").addEventListener("click", registrarSolicitante); 
 
-// Funcion para listar solicitantes de prestamos
-const listarSolicitantes = () => {
+// Event Listener para el botón de listar solicitantes 
+document.getElementById("listar").addEventListener("click", () => {
   // Obtener la lista de solicitantes guardada en el local storage
   const solicitantesGuardados = localStorage.getItem("solicitantes");
-
   // Verificar si hay solicitantes guardados
-  if (solicitantesGuardados) {
-    const listaSolicitantes = JSON.parse(solicitantesGuardados);
-    const listaSolicitantesDiv = document.getElementById("listaSolicitantes");
-    //  Limpiar la lista de solicitantes antes de mostrarla
-    listaSolicitantesDiv.innerHTML = "";
-
-    //  Crear una lista para mostrar los solicitantes
-    const ul = document.createElement("ul");
-
-    // Crear un li por cada solicitante y agregarlo a la lista
-    listaSolicitantes.forEach((solicitante) => {
-      const li = document.createElement("li");
-      li.textContent = ` Nombre: ${solicitante.nombre}, Apellido: ${solicitante.apellido},
-                         Documento: ${solicitante.documento}, Edad: ${solicitante.edad}`;
-      ul.appendChild(li);
-    });
-
-    // Mostrar la lista de solicitantes en el HTML
-    listaSolicitantesDiv.appendChild(ul);
-
-    } else {
-      // Mostrar un mensaje tipo alert usando Toastify
-      Toastify({
-        text: "No hay solicitantes registrados",
-        duration: 3000,
-        newWindow: true,
-        close: false,
-        gravity: "top", 
-        position: "right", 
-        stopOnFocus: true, 
-        style: {
-          background: "linear-gradient(to right, #000ff, #87cefa)",
-        },
-      }).showToast();
-    }
-  } ; 
-
-
-  
-  const redirigirListaSolicitantes = () => {
+  if ( !solicitantesGuardados || JSON.parse(solicitantesGuardados).length === 0) {
+    // Si no hay solicitantes guardados, mostrar un mensaje tipo alert usando Toastify
+    Toastify({
+      text: "No hay solicitantes registrados",
+      duration: 3000,
+      newWindow: true,
+      close: false,
+      gravity: "top",
+      position: "right",
+      stopOnFocus: true,
+      style: {
+        background: "linear-gradient(to right, #000ff, #87cefa)",
+      },
+    }).showToast();
+  } else {
+    // Si hay solicitantes guardados, redirigir a la página de lista.html para mostrarlos
     window.location.href = "lista.html";
-  } ;
-
-  document.getElementById("listar").addEventListener("click", redirigirListaSolicitantes);
-  // Event Listener para el boton de Listar solicitantes
-  // document.getElementById("listar").addEventListener("click", listarSolicitantes);
+  }
+});
 
 
 
